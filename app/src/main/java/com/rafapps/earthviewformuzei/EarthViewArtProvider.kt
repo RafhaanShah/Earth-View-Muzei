fcf86e8dc11cff6dc1b2901c91121535e65e7998
Rafhaan Shah <RafhaanShah@users.noreply.github.com>
2019-07-05 11:42:24 +0530

    Initial commit

---
+++ app/src/main/java/com/rafapps/earthviewformuzei/EarthViewArtProvider.kt
@@ -0,0 +1,9 @@
+package com.rafapps.earthviewformuzei
+
+import com.google.android.apps.muzei.api.provider.MuzeiArtProvider
+
+class EarthViewArtProvider : MuzeiArtProvider() {
+    override fun onLoadRequested(initial: Boolean) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ app/src/main/java/com/rafapps/earthviewformuzei/EarthViewArtSource.kt
@@ -0,0 +1,13 @@
+package com.rafapps.earthviewformuzei
+
+import com.google.android.apps.muzei.api.RemoteMuzeiArtSource
+
+class EarthViewArtSource : RemoteMuzeiArtSource("Name") {
+
+
+    override fun onTryUpdate(reason: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
